561c24f286242a6399d44cef218bc0375a9cd099
SAUTH.PHOUEK <SAUTH.PHOUEK@pnc.lan>
2022-04-28 06:56:27 +0700

    front end

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 3000
+
+
+app.use(cors({origin: "*"}));
+app.use(express.static("public"))
 
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT)
