01f304a5437fe2ef8b90ef1b94e0e861d623ce84
VEANGLY <veang.kroh#student.passerellesnumerique.org>
2022-04-28 09:19:22 +0700

    #7 display question for back end

--- routes/question_route.js
+++ routes/question_route.js
@@ -10,22 +10,4 @@ router.get("/display_question", (req, res) => {
     .then((result) =>res.send(result))
     .catch((error) =>res.send(error))
 });
-// Add question to MongoDB
-router.post("/add_question", (req, res) => {
-    questionsModel.create(req.body)
-    .then((result) =>res.send(result))
-    .catch((error) =>res.send(error))
-});
-// Delete question to MongoDB
-router.delete('/delete_question/:id',(req, res) => {
-    questionsModel.deleteOne({_id:req.params.id})
-    .then((result) =>res.send(result))
-    .catch((error) =>res.send(error))
-});
-// Update question to MongoDB
-router.put('/update_question/:id',(req, res) => {
-    questionsModel.updateOne({_id:req.params.id},req.body)
-    .then((result) =>res.send(result))
-    .catch((error) =>res.send(error))
-})
 module.exports = router;
