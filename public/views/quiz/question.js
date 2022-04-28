fa7441b280c18fb4be0f0f1e769bed6bb1f0ed85
SAUTH.PHOUEK <SAUTH.PHOUEK@PNC.LAN>
2022-04-28 07:39:18 +0700

    display question #7

--- public/views/quiz/question.js
+++ public/views/quiz/question.js
@@ -1,7 +1,11 @@
 
 function removeDOM(e){
     e.target.parentElement.parentElement.remove();
-    playQuiz();
+    if (e.target.id == "play_quiz"){
+        playQuiz();
+    } else{
+        showAllQuestion();
+    }
 }
 
 function playQuiz() {
@@ -54,6 +58,70 @@ function playQuiz() {
         console.log(list_of_questions[0]["answers"]["A"]);
         // INCREMENT INDEX BY 1
         index += 1;
+        document.body.appendChild(content_question);
+        
+    }
+}
+
+function showAllQuestion(){
+    for (let index=0;index<list_of_questions.length;index++){
+        // GET QUESTION FROM ARRAY OF OBJECTS
+        let content_question  = document.createElement("div");
+        content_question.classList = "container-fluid w-50";
+        let card = document.createElement("div");
+        card.className = "question";
+        let question = document.createElement("h4")
+        question.textContent = index+1 + "/ " + list_of_questions[index]['question'];
+        card .appendChild(question);
+        // CHANGE ANSWER ALL TIME WHENEVER USER CLICK NEXT
+        // // CREATE LIST FOR ANSWER-1
+        let content_answers = document.createElement("div");
+        content_answers.classList = "answers";
+
+        let box1 = document.createElement("div");
+        box1.classList = "box d-flex";
+
+        let answer_1 = document.createElement("div");
+        answer_1.classList = "btn btn-primary";
+        answer_1.textContent = list_of_questions[index]["answers"]["A"];
+        
+        // CREATE LIST FOR ANSWER-2
+        let answer_2 = document.createElement("div");
+        answer_2.classList = "btn btn-primary";
+        answer_2.textContent = list_of_questions[index]["answers"]["B"];
+        
+        box1.appendChild(answer_1);
+        box1.appendChild(answer_2);
+        let box2 = document.createElement("div");
+        box2.classList = "box d-flex";
+        // CREATE LIST FOR ANSWER-3
+        let answer_3 = document.createElement("div");
+        answer_3.classList = "btn btn-primary";
+        answer_3.textContent = list_of_questions[index]["answers"]["C"];
+
+        // CREATE LIST FOR ANSWER-4
+        let answer_4 = document.createElement("div");
+        answer_4.classList = "btn btn-primary";
+        answer_4.textContent = list_of_questions[index]["answers"]["D"];
+        box2.appendChild(answer_3);
+        box2.appendChild(answer_4);
+
+        let card_footer = document.createElement("div");
+        card_footer.className = "card_footer";
+        let btn_edit = document.createElement("div");
+        btn_edit.className = "edit_question";
+        btn_edit.textContent = "Edit";
+        let btn_delete = document.createElement("div");
+        btn_delete.className = "delete_question"
+        btn_delete.textContent = "Delete";
+        card_footer.appendChild(btn_edit);
+        card_footer.appendChild(btn_delete);
+        content_answers.appendChild(box1);
+        content_answers.appendChild(box2);
+        content_question.appendChild(card);
+        content_question.appendChild(content_answers);
+        content_question.appendChild(card_footer);
+        // INCREMENT INDEX BY 1
         document.body.appendChild(content_question);
         
     }
